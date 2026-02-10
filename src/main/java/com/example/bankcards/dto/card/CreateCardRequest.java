676c7587081647307f120e515d74a3817763e98e
Timur Ainetdinov <tainetdinov@itoolabs.com>
2026-02-10 17:49:09 +0300

    Added ReadMe

--- src/main/java/com/example/bankcards/dto/card/CreateCardRequest.java
+++ src/main/java/com/example/bankcards/dto/card/CreateCardRequest.java
@@ -1,6 +1,7 @@
 package com.example.bankcards.dto.card;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
@@ -8,6 +9,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 
 @Data
@@ -26,4 +28,8 @@ public class CreateCardRequest {
     @FutureOrPresent
     @JsonFormat(pattern = "dd.MM.yyyy")
     private LocalDate expirationDate;
+
+    @NotNull(message = "Balance must be provided")
+    @DecimalMin(value = "0.01", message = "Balance must be greater than 0")
+    private BigDecimal balance;
 }
